1451baeacc4f145550c34ac43cacc78581d8517a
Mochamad Alghfari <alghifarim@garena.co.id>
2019-05-31 18:40:18 +0700

    Add register component

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
-import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 import BootstrapVue from 'bootstrap-vue'
 import VueRouter from 'vue-router'
 import router from './routes'
--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,15 @@
 import VueRouter from 'vue-router'
-import HelloWorld from "./components/HelloWorld";
+import HelloWorld from "./components/HelloWorld"
+import Register from "./components/Register"
 
 const routes = [
     {
         path: '/',
         component: HelloWorld
+    },
+    {
+        path: '/register',
+        component: Register
     }
 ];
 
