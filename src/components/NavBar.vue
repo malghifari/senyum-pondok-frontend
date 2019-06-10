af2f9c10eee04b6d8b0b8d20f7369f4b204477c8
Mochamad Alghfari <alghifarim@garena.co.id>
2019-06-10 12:16:33 +0700

    Fixed Navbar with real png logo

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -3,21 +3,9 @@
         <b-navbar class="main-navbar" toggleable="lg" type="dark">
             <b-navbar-brand href="#">
                 <b-row>
-                    <b-col class="brand-senyum-indonesia" cols="12">
-                        <b-row>
-                            <b-col class="col-logo">
-                                <img src="../assets/logo-senyum-putih.png" class="logo">
-                            </b-col>
-                            <b-col class="brand-name">
-                                <b-row class="brand-name-senyum">
-                                    Senyum
-                                </b-row>
-                                <b-row class="brand-name-indonesia">
-                                    Indonesia
-                                </b-row>
-                            </b-col>
-                        </b-row>
-                    </b-col>
+                    <div class="brand-senyum-indonesia">
+                        <img src="../assets/logo-senyum-indonesia-putih.png" class="logo">
+                    </div>
                 </b-row> 
             </b-navbar-brand>
         </b-navbar>
@@ -36,38 +24,11 @@ export default {
     }
 
     .logo {
-        width: 6vh;
-        height: auto;
-        margin-right: 0px;
-    }
-
-    .brand-name {
-        font-size: 13px;
-
-    }
-    .brand-name-senyum {
-        font-weight: bold
-    }
-
-    .col-logo {
-        padding-right: 3px;
+        width: 100px;
+        margin-left: 10vh;
+        padding: 0px;
     }
 
     @media screen and (min-width: 540px) {
-        .brand-name {
-            padding-top: 4px;
-            padding-left: 6px;
-            font-size: 17px;
-        }
-        .brand-senyum-indonesia {
-            margin-left: 250px;
-        }
-        .brand-name-senyum {
-            font-weight: bold;
-        }
-        .col-logo {
-            flex-grow: 0.2;
-            margin-right: 15px; 
-        }
     }
 </style>
