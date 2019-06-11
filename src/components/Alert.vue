9336d3b3d450a440a1d4c5f3f5e544aa0fcdeda7
Mochamad Alghifari <mo.alghifari@gmail.com>
2019-06-11 14:42:03 +0700

    Add Alert component

---
+++ src/components/Alert.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="main-alert">
+    <b-alert variant="danger" show>
+        <div class="alert-bound">
+            {{ message }}
+        </div>
+    </b-alert>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['message'],
+};
+</script>
+
+<style scoped>
+    .main-alert {
+        text-align: center;
+    }
+    .alert-bound {
+        padding-left: 20px;
+        margin: auto;
+    }
+    @media screen and (min-width: 1366px) {
+        .alert-bound {
+            width: 1366px;
+        }
+    }
+</style>
