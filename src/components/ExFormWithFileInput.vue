4afeef79cccd6445c6fe888fa70311a07e9c4e0b
Mochamad Alghifari <mo.alghifari@gmail.com>
2019-06-27 09:36:43 +0700

    Fail

--- src/components/ExFormWithFileInput.vue
+++ src/components/ExFormWithFileInput.vue
@@ -26,19 +26,35 @@
                 </b-form-input>
             </b-form-group>
 
-            <b-form-group 
-                id="input-group-3"
-                >
-                <b-form-file 
-                    accept="image/jpeg, image/png, image/gif"
-                    required
-                    v-model="form.file"
-                    :state="Boolean(form.file)"
-                    placeholder="Pilih file..."
-                    drop-placeholder="Taruh file disini..."
+            <image-uploader
+                :preview="true"
+                :className="['fileinput', { 'fileinput--loaded': hasImage }]"
+                capture="environment"
+                :debug="1"
+                doNotResize="gif"
+                :autoRotate="true"
+                outputFormat="verbose"
+                @input="setImage"
+            >
+                <label for="fileInput" slot="upload-label">
+                <figure>
+                    <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="32"
+                    height="32"
+                    viewBox="0 0 32 32"
                     >
-                </b-form-file>  
-            </b-form-group>
+                    <path
+                        class="path1"
+                        d="M9.5 19c0 3.59 2.91 6.5 6.5 6.5s6.5-2.91 6.5-6.5-2.91-6.5-6.5-6.5-6.5 2.91-6.5 6.5zM30 8h-7c-0.5-2-1-4-3-4h-8c-2 0-2.5 2-3 4h-7c-1.1 0-2 0.9-2 2v18c0 1.1 0.9 2 2 2h28c1.1 0 2-0.9 2-2v-18c0-1.1-0.9-2-2-2zM16 27.875c-4.902 0-8.875-3.973-8.875-8.875s3.973-8.875 8.875-8.875c4.902 0 8.875 3.973 8.875 8.875s-3.973 8.875-8.875 8.875zM30 14h-4v-2h4v2z"
+                    ></path>
+                    </svg>
+                </figure>
+                <span class="upload-caption">{{
+                    hasImage ? "Replace" : "Click to upload"
+                }}</span>
+                </label>
+            </image-uploader>
 
             <b-button block type="submit" style="background-color: #d71149; border-color:  #d71149; border-radius: 3px">KIRIM</b-button>
         </b-form>
@@ -57,6 +73,8 @@ export default {
                 password: '',
                 file: null
             },
+            hasImage: false,
+            image: null
         }
     },
     methods: {
@@ -65,7 +83,7 @@ export default {
             const path = process.env.VUE_APP_BASE_API + 'transaction/create';
             axios.post(path, payload, {
                 headers: {
-                    'Content-Type': 'multipart/form-data'
+                    'Content-Type': 'application/json'
                 }
             })
             .then((res) => {
@@ -77,17 +95,22 @@ export default {
         },
         onSubmit(evt) {
             evt.preventDefault();
-            if (this.form.file.size < 2000000) {
-                const payload = {
-                    whatsapp: this.form.whatsapp,
-                    password: this.form.password,
-                    file: this.form.file
-                };
-                this.submit(payload);
-            } else {
-                console.log("Ukuran maksimal 2MB")
-            }
+            // if (this.image.size < 2000000000) {
+            const payload = {
+                whatsapp: this.form.whatsapp,
+                password: this.form.password,
+                file: this.image
+            };
+            this.submit(payload);
+            // } else {
+            //     console.log("Ukuran maksimal 2MB")
+            // }
         },
+        setImage: function(output) {
+            this.hasImage = true;
+            this.image = output;
+            console.log(this.image);
+        }
     }
 }
 </script>
